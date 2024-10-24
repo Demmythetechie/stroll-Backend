2aee8c6f6607dce4d9b0c39a605c89b6368ce4f7
Demmythetechie <demmythetechie@gmail.com>
2024-10-24 19:32:16 +0100

    Backend Fifth

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import e from 'express';
 import cors from 'cors';
 const app = e();
 
-app.use(cors());
+const corsOptions ={
+    origin:'https://stroll-naheem-okunades-projects.vercel.app/', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 // Define a simple route
 app.post('/user', (req, res) => {
