1fdfb3761ebfd613c734311f8ca2059cdeaa78a6
Demmythetechie <demmythetechie@gmail.com>
2024-10-24 19:03:51 +0100

    Backend Fifth

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ import e from 'express';
 import cors from 'cors';
 const app = e();
 
-app.use(cors({
-    origin: 'http://localhost:3000', // Allow only your frontend origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allow these HTTP methods
-    credentials: true,               // Enable cookies/sessions if needed
-}));
+app.use(cors());
 
 // Define a simple route
 app.post('/user', (req, res) => {
