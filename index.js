a4e56ff2ac3449dd4de87c5d24cc21b43f6affd6
Demmythetechie <demmythetechie@gmail.com>
2024-10-24 23:03:58 +0100

    Backend Fifth

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const app = express();
 app.use(cors());
 
 // Define a simple route
-app.post('/user', (req, res) => {
+app.post('/api/user', (req, res) => {
     console.log(req.body);
 });
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.send('Hello from Express!');
 });
 
