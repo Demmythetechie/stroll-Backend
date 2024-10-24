1c5b768b3f0a020473c5a1008e09c18053d257e5
Demmythetechie <demmythetechie@gmail.com>
2024-10-24 19:00:30 +0100

    backend fourth

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
+import e from 'express';
+import cors from 'cors';
+const app = e();
 
 app.use(cors({
     origin: 'http://localhost:3000', // Allow only your frontend origin
-    methods: 'GET,POST,PUT,DELETE',  // Allow these HTTP methods
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Allow these HTTP methods
     credentials: true,               // Enable cookies/sessions if needed
 }));
 
@@ -17,8 +17,4 @@ app.get('/', (req, res) => {
     res.send('Hello from Express!');
 });
 
-// Start the server
-const port = 4000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+app.listen(4000)
